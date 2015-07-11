aac2a9506807b3c7fd9aa6eeb029a0e25126f02e
sk8ersteve <stephen.bulldog.thomas3@gmail.com>
2015-07-11 11:48:40 -0400

    Adding the repo

---
+++ Assets/Scripts/enemyManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyManager : MonoBehaviour {
+
+	public Transform prefab;
+	public int enemyCount;
+	public Vector2 spawnPosition;
+	public int maxSpeed;
+	
+
+
+	// Use this for initialization
+	void Start () {
+		SpawnEnemys ();
+	}
+
+	void SpawnEnemys () {
+		for (int i = enemyCount; i>0; i--) {
+			Transform enemy = (Transform)Instantiate(prefab);
+			Rigidbody2D rgbd = enemy.GetComponent<Rigidbody2D> ();
+			// Determines which edge of screen enemy spawns from
+			if (i%4 == 0) {
+				spawnPosition = new Vector2(Random.Range(-10,10),5);
+				enemy.localPosition = spawnPosition;
+				rgbd.velocity = new Vector2(Random.Range(-maxSpeed,maxSpeed),Random.Range(-maxSpeed,-maxSpeed/2));
+			}
+			if (i%4 == 1) {
+				spawnPosition = new Vector2(10,Random.Range(-5,5));
+				enemy.localPosition = spawnPosition;
+				rgbd.velocity = new Vector2(Random.Range(-maxSpeed,-maxSpeed/2),Random.Range(-maxSpeed,maxSpeed));
+			}
+			if (i%4 == 2) {
+				spawnPosition = new Vector2(Random.Range(-10,10),-5);
+				enemy.localPosition = spawnPosition;
+				rgbd.velocity = new Vector2(Random.Range(-maxSpeed,maxSpeed),Random.Range(maxSpeed/2,maxSpeed));
+			}
+			if (i%4 == 3) {
+				spawnPosition = new Vector2(-10,Random.Range(-5,5));
+				enemy.localPosition = spawnPosition;
+				rgbd.velocity = new Vector2(Random.Range(maxSpeed/2,maxSpeed),Random.Range(-maxSpeed,maxSpeed));
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/enemyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.localPosition.x > 10 || transform.localPosition.x < -10)
+			transform.localPosition = new Vector2(-transform.localPosition.x,transform.localPosition.y);
+		if (transform.localPosition.y > 5 || transform.localPosition.y < -5)
+			transform.localPosition = new Vector2(transform.localPosition.x, -transform.localPosition.y);
+
+	}
+}
